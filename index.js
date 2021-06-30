a1268d507f99e1c089ec6496d20a6a60fa8880d9
--add <andrewksmoker@gmail.com>
2021-06-30 18:25:18 -0400

    First function

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function findMinAndRemove(array){
-
+    let min = array[0]
+    for (let i = 0;i<array.length - 1;i++) {
+        if (array[i] < min) {
+            min = array[i]
+            console.log(min)
+        }
+    }
+    return min
 }
 
 function selectionSort(array){
